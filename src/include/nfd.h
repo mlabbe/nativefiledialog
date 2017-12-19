0ff0bfb96a89941fb5201135f6c8973885556152
Len Remmerswaal <len.mail1@xs4all.nl>
2017-12-20 00:42:44 +0100

    Make extra functions to support user supplied dialog titles
    Signed-off-by: Len Remmerswaal <len.mail1@xs4all.nl>

--- src/include/nfd.h
+++ src/include/nfd.h
@@ -35,23 +35,42 @@ typedef enum {
 
 /* nfd_<targetplatform>.c */
 
-/* single file open dialog */    
+/* single file open dialog */ 
+nfdresult_t NFD_OpenDialogTitled( const nfdchar_t *title,
+							      const nfdchar_t *filterList,
+                                  const nfdchar_t *defaultPath,
+                                  nfdchar_t **outPath );
+                                     
 nfdresult_t NFD_OpenDialog( const nfdchar_t *filterList,
                             const nfdchar_t *defaultPath,
                             nfdchar_t **outPath );
 
 /* multiple file open dialog */    
+nfdresult_t NFD_OpenDialogMultipleTitled( const nfdchar_t *title,
+									      const nfdchar_t *filterList,
+                                          const nfdchar_t *defaultPath,
+                                          nfdpathset_t *outPaths );
+
 nfdresult_t NFD_OpenDialogMultiple( const nfdchar_t *filterList,
                                     const nfdchar_t *defaultPath,
                                     nfdpathset_t *outPaths );
 
 /* save dialog */
+nfdresult_t NFD_SaveDialogTitled( const nfdchar_t *title,
+								  const nfdchar_t *filterList,
+                                  const nfdchar_t *defaultPath,
+                                  nfdchar_t **outPath );
+
 nfdresult_t NFD_SaveDialog( const nfdchar_t *filterList,
                             const nfdchar_t *defaultPath,
                             nfdchar_t **outPath );
 
 
 /* select folder dialog */
+nfdresult_t NFD_PickFolderTitled( const nfdchar_t *title,
+								  const nfdchar_t *defaultPath,
+								  nfdchar_t **outPath);
+
 nfdresult_t NFD_PickFolder( const nfdchar_t *defaultPath,
                             nfdchar_t **outPath);
 
